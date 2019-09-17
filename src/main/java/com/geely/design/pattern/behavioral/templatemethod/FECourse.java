f68411e48d069618e174bcca102331ed5e99aa0c
wzy <wangzy0327@qq.com>
2019-09-17 17:25:00 +0800

    add template method pattern

---
+++ src/main/java/com/geely/design/pattern/behavioral/templatemethod/FECourse.java
@@ -0,0 +1,21 @@
+package com.geely.design.pattern.behavioral.templatemethod;
+
+public class FECourse extends ACourse {
+
+    private boolean needWriteArticleFlag = false;
+
+    public FECourse(boolean needWriteArticleFlag) {
+        this.needWriteArticleFlag = needWriteArticleFlag;
+    }
+
+    @Override
+    void packageCourse() {
+        System.out.println("提供课程的前端代码");
+        System.out.println("提供课程的图片等多媒体素材");
+    }
+
+    @Override
+    protected boolean needWriteArticle() {
+        return this.needWriteArticleFlag;
+    }
+}
